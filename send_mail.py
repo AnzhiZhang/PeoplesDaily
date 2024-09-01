d34b24be85f705c3cd00d40bdb8e9a574e49187c
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2024-09-01 19:52:00 +0100

    feat: ✨ add send_email

---
+++ send_mail.py
@@ -0,0 +1,156 @@
+import os
+import smtplib
+import argparse
+import datetime
+
+import markdown2
+
+from email import encoders
+from email.header import Header
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.utils import parseaddr, formataddr
+
+from main import TodayPeopleDaily
+
+__all__ = [
+    'send_email',
+]
+
+DAY_OF_WEEK = ['一', '二', '三', '四', '五', '六', '日']
+
+
+def format_addr(s):
+    name, addr = parseaddr(s)
+    return formataddr((name, addr))
+
+
+def send_email(
+        smtp_server: str,
+        smtp_port: int,
+        smtp_ssl: bool,
+        smtp_user: str,
+        smtp_password: str,
+        sender: str,
+        recipients: list[str]
+):
+    # connect smtp server
+    if smtp_ssl:
+        server = smtplib.SMTP_SSL(smtp_server, smtp_port)
+    else:
+        server = smtplib.SMTP(smtp_server, smtp_port)
+    server.login(smtp_user, smtp_password)
+
+    # get today peoples daily
+    today_peoples_daily = TodayPeopleDaily()
+    today_peoples_daily.get_today_peoples_daily()
+    date = datetime.datetime.strptime(today_peoples_daily.date, '%Y-%m-%d')
+
+    # construct email
+    msg = MIMEMultipart()
+    msg['From'] = format_addr(sender)
+    msg['Subject'] = Header(
+        (
+            f'人民日报 '
+            f'{date.year}年{str(date.month).zfill(2)}月'
+            f'{str(date.day).zfill(2)}日 '
+            f'星期{DAY_OF_WEEK[date.weekday()]}'
+        ),
+        'utf-8'
+    ).encode()
+    msg.attach(MIMEText(
+        markdown2.markdown(today_peoples_daily.release_body),
+        'html',
+        'utf-8'
+    ))
+
+    # attach pdf
+    pdf_name = os.path.basename(today_peoples_daily.merged_file_path)
+    with open(today_peoples_daily.merged_file_path, 'rb') as f:
+        mime = MIMEBase(
+            'application',
+            'pdf',
+            filename=pdf_name
+        )
+        mime.add_header(
+            'Content-Disposition',
+            'attachment',
+            filename=pdf_name
+        )
+        mime.add_header('Content-ID', '<0>')
+        mime.add_header('X-Attachment-Id', '0')
+        mime.set_payload(f.read())
+        encoders.encode_base64(mime)
+        msg.attach(mime)
+
+    # send email
+    for recipient in recipients:
+        msg['To'] = format_addr(recipient)
+        server.sendmail(sender, [recipient], msg.as_string())
+
+    # quit server
+    server.quit()
+
+
+def main():
+    # setup parser
+    parser = argparse.ArgumentParser(
+        description="Scheduled mail People's Daily",
+    )
+    parser.add_argument(
+        "--smtp-server",
+        help="SMTP server",
+    )
+    parser.add_argument(
+        "--smtp-port",
+        help="SMTP port",
+    )
+    parser.add_argument(
+        "--smtp-ssl",
+        action=argparse.BooleanOptionalAction,
+        default=False,
+        help="SMTP SSL",
+    )
+    parser.add_argument(
+        "--smtp-user",
+        help="SMTP user",
+    )
+    parser.add_argument(
+        "--smtp-password",
+        help="SMTP password",
+    )
+    parser.add_argument(
+        "--sender",
+        help="Sender",
+    )
+    parser.add_argument(
+        "--recipients",
+        help="Recipients",
+        nargs='+',
+    )
+
+    # parse arguments
+    args = parser.parse_args()
+    smtp_server = args.smtp_server
+    smtp_port = args.smtp_port
+    smtp_ssl = args.smtp_ssl
+    smtp_user = args.smtp_user
+    smtp_password = args.smtp_password
+    sender = args.sender
+    recipients = args.recipients
+
+    # send email
+    send_email(
+        smtp_server,
+        smtp_port,
+        smtp_ssl,
+        smtp_user,
+        smtp_password,
+        sender,
+        recipients
+    )
+
+
+if __name__ == '__main__':
+    main()
