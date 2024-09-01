cc1fe0e85ca68599943efc1d59c20e5b03bb7214
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2024-09-01 20:58:24 +0100

    feat: ✨ add schedule_task

---
+++ schedule_task.py
@@ -0,0 +1,56 @@
+import os
+import datetime
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+from peoples_daily import TodayPeopleDaily
+from send_email import EmailConfig, send_email
+
+
+def get_today_peoples_daily(email_config: EmailConfig):
+    # get today peoples daily
+    today_peoples_daily = TodayPeopleDaily()
+    today_peoples_daily.get_today_peoples_daily()
+    print(f"Got People's Daily for {today_peoples_daily.date}")
+
+    # send email
+    if len(email_config.recipients) != 0:
+        send_email(
+            email_config,
+            today_peoples_daily
+        )
+        print(f"Sent email to {email_config.recipients}")
+
+
+def main():
+    # mail config
+    smtp_port = int(os.environ.get('SMTP_PORT', 0))
+    smtp_ssl = os.environ.get('SMTP_SSL', 'False').lower() == 'true'
+    recipients = os.environ.get('RECIPIENTS', '').split(",")
+    recipients = [r.strip() for r in recipients if r.strip() != '']
+    mail_config = EmailConfig(
+        os.environ.get('SMTP_SERVER', ''),
+        smtp_port,
+        smtp_ssl,
+        os.environ.get('SMTP_USER', ''),
+        os.environ.get('SMTP_PASSWORD', ''),
+        os.environ.get('SENDER', ''),
+        recipients
+    )
+
+    # build scheduler
+    scheduler = BlockingScheduler(timezone=datetime.UTC)
+    scheduler.add_job(
+        get_today_peoples_daily,
+        'cron',
+        hour='0',
+        minute='0',
+        args=(mail_config,)
+    )
+
+    # start scheduler
+    scheduler.start()
+
+
+if __name__ == '__main__':
+    main()
